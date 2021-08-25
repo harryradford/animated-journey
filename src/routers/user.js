f4b6f64485969d278acb19168f1975fdf7adf029
harryradford <harry.radford@live.co.uk>
2021-08-25 18:39:03 +0100

    Read an avatar by user id

--- src/routers/user.js
+++ src/routers/user.js
@@ -132,5 +132,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     }
 })
 
+// Read an avatar by user id.
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.status(200).send(user.avatar)
+    } catch (error) {
+        res.status(404).send()
+    }
+})
+
 // Module exports.
 export default router
