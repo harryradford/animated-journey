ad6105c2cd4bb31e5be529ffe581a9f7d39cbf4b
harryradford <harry.radford@live.co.uk>
2021-09-07 10:25:53 +0100

    Test getting user profile

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,15 +1,22 @@
 // Import npm modules.
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 
 // Import local modules.
 const app = require('../src/app.js')
 const User = require('../src/models/user.js')
 
 // Prepare the database before each test case.
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'Arthur',
     email: 'arthur@example.com',
-    password: 'testpass'
+    password: 'testpass',
+    tokens: [{
+        token: jwt.sign({_id: userOneId}, process.env.JWT_SECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -91,4 +98,20 @@ test('Should login an existing user', async () => {
         email: userOne.email,
         password: userOne.password
     }).expect(200)
+})
+
+// Test getting a user profile.
+test('Should get user profile', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('Should not get user profile when unauthenticated', async () => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
 })
